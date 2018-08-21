5a7faa2d3a12f7334db5527053afbeb9711b26e8
Jacob Perry <rakoryn@outlook.com>
2018-08-20 18:16:15 -0700

    Smooth Scroll, Front end Section and more... cause I suck at using git right

--- updated/js/app.js
+++ updated/js/app.js
@@ -9,4 +9,30 @@ window.addEventListener("scroll", e => {
     } else {
         $("nav").removeClass("background");
     }
+
+    if(window.scrollY < 900) {
+        $(".header").css("background-position-y", window.scrollY / 1.25);
+        $("#logo").css("transform", `translateY(${-window.scrollY / 2}px)`);
+        $("#header-text").css("transform", `translateY(${-window.scrollY / 2}px)`);
+    }
 })
+
+
+$("a").on('click', function(event) {
+    event.preventDefault();
+
+    let hash = event.currentTarget.hash;
+
+    if (hash != '' && $(hash).position()) {
+        window.scrollTo({
+            top: $(hash).position().top,
+            behavior: "smooth"
+        })
+
+    } else {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+});
