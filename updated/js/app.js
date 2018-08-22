990f2f67441458c91ba1eb022032b4e39a21a392
Jacob Perry <rakoryn@outlook.com>
2018-08-22 02:13:38 -0700

    Added MIT License, Adjusted ScrollTo and Added Blinking Cursor

--- updated/js/app.js
+++ updated/js/app.js
@@ -4,35 +4,28 @@ $(".nav-item a").click(e => {
 })
 
 window.addEventListener("scroll", e => {
-    if(window.scrollY > 100) {
+    if (window.scrollY > 100) {
         $("nav").addClass("background");
     } else {
         $("nav").removeClass("background");
     }
 
-    if(window.scrollY < 900) {
+    if (window.scrollY < 900) {
         $(".header").css("background-position-y", window.scrollY / 1.25);
         $("#logo").css("transform", `translateY(${-window.scrollY / 2}px)`);
         $("#header-text").css("transform", `translateY(${-window.scrollY / 2}px)`);
     }
 })
 
+/* Smooth Srolling Code from Julia Hide on Codepen */
 
-$("a").on('click', function(event) {
-    event.preventDefault();
-
-    let hash = event.currentTarget.hash;
-
-    if (hash != '' && $(hash).position()) {
-        window.scrollTo({
-            top: $(hash).position().top,
-            behavior: "smooth"
-        })
-
-    } else {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })
+$('.nav-link').click(function (e) {
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if (target.length) {
+        var scrollTo = target.offset().top;
+        $('html, body').animate({
+            scrollTop: (scrollTo - 50) + 'px'
+        }, 1250);
     }
 });
