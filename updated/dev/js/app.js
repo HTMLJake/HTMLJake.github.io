3c92e636e5f333e9d90e02d489649e1bb47b14c6
Jacob Perry <btjake@TheMetalFruit.home>
2018-09-25 04:39:31 -0700

    Switch to full bootstrap Navbar

--- updated/dev/js/app.js
+++ updated/dev/js/app.js
@@ -1,56 +1,7 @@
-let sidebarDelay = 200;
-
-$(".nav-item a").click((e) => {
-	$(".active").toggleClass("active");
-	e.currentTarget.classList.toggle("active");
-});
-
-window.addEventListener("scroll", () => {
-	if (window.scrollY > 100) {
-		$("nav").addClass("background");
+$(window).scroll(() => {
+	if (window.scrollY > 25) {
+		$(".navbar").addClass("navbar-dark").addClass("bg-dark").removeClass("navbar-light");
 	} else {
-		$("nav").removeClass("background");
+		$(".navbar").removeClass("navbar-dark").removeClass("bg-dark").addClass("navbar-light");
 	}
 });
-
-/* Smooth Srolling Code from Julia Hide on Codepen */
-
-$(".nav-link").click(function (e) {
-	//e.preventDefault();
-
-	if (window.innerWidth > 825) {
-		sidebarDelay = 0;
-	} else {
-		closeSidebar();
-		sidebarDelay = 200;
-	}
-
-	setTimeout(() => {
-		var target = $($(this).attr("href"));
-		if (target.length) {
-			var scrollTo = target.offset().top - 70;
-			$("html, body").animate({
-				scrollTop: scrollTo + "px"
-			}, 1250);
-		}
-	}, sidebarDelay);
-});
-
-/*eslint no-unused-vars: "off"*/
-
-function toggleSidebar(hamburger) {
-	const sidebarElem = window.document.getElementById("sidebar");
-	sidebarElem.classList.toggle("open");
-	hamburger.classList.toggle("open");
-}
-
-function closeSidebar() {
-	const sidebarElem = window.document.getElementById("sidebar");
-	const hamburger = window.document.getElementById("hamburger");
-	sidebarElem.classList.remove("open");
-	hamburger.classList.remove("open");
-}
-
-function nerfPLS(itemToNerf) {
-	window.alert("I would like to formally request a nerf to " + itemToNerf + ". Thank You.");
-}
