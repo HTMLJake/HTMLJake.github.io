d78b17284ff99d80ab98ad236219eb8d92ad6a5b
Jacob Perry <rakoryn@outlook.com>
2018-09-27 03:47:41 -0700

    Added project cards with Vue.js

--- updated/dev/js/app.js
+++ updated/dev/js/app.js
@@ -1,4 +1,3 @@
-
 $(window).scroll(() => {
 	if (window.scrollY > 50) {
 		$(".navbar").addClass("navbar-dark bg-dark").removeClass("navbar-light bg-light");
@@ -8,3 +7,35 @@ $(window).scroll(() => {
 		$(".bar").addClass("bg-dark").removeClass("bg-light");
 	}
 });
+
+
+
+let app = new Vue({
+	el: "#cards",
+	data: {
+		state: 0,
+		message: "Hello World",
+		cards: [
+			{
+				title: "Just a title",
+				img: "../dev/img/voxel-jacob.png",
+				text: "I like to make stuff, learn things, and speak vague. Some of the hats that I wear are Frontend Developer, Game Developer, Graphic Designer, Musician, and Photographer."
+			},
+		],
+	},
+	methods: {
+		ChangeState: function (i) {
+			this.state = i;
+		}
+	}
+});
+
+
+
+(function fetchJson () {
+	fetch("../dev/js/data.json").then((r) => {
+		return r.json();
+	}).then(j => {
+		app.cards = j;
+	});
+}());
