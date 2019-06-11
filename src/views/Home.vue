0756c06ddf5793bbdd0efaaadc33d28fe85ffa3b
Jacob Perry <rakoryn@outlook.com>
2019-06-11 03:02:43 -0700

    Updated about me text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,9 +14,9 @@
 
       <div class="about-text">
         <p>
-          You are probably looking at this site for one of a few reasons. Either you are trying to see if I am someone you think is worth hiring, or its my mom checking out my site to see how it looks. Either way I make to make a good impression and hope that you like the way things look, or at least maybe you will chuckle. Like when you watch those videos of a cat playing the piano, its just so adorable 😍. Wait, getting off topic... <em>Alright jacob we gotta look professional, pull it together!</em> Anyways, I look forward to hearing what you think.
+          You are probably looking at this site for one of a few reasons. Either you are trying to see if I am someone you think is worth hiring, or its my mom checking out my site to see how it looks. If it is my mom, Hi Mom, if not well I hope this site will help you figure out if I am someone you would want working for you, or at least maybe you will chuckle. Like when you watch those videos of a cat playing the piano, its just so adorable 😍. Wait, getting off topic... <em>Alright jacob we gotta look professional, pull it together!</em> Thank you for taking the time to check out my site. I appreciate the time you are here and please keep all hands, feet inside the vehicle at all times, oh and enjoy.
 
-          But really though, you should go check out those cat videos. 10/10, would watch again. You know what, here is a gif of it, just for you.
+          ...But really though, you should go check out those cat videos. 10/10, would watch again. You know what, here is a gif of it, just for you.
         </p>
         <img
           src="https://media.giphy.com/media/10RhccNxPSaglW/giphy.gif"
