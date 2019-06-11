33d9954d540f7474fdfeaf5be981e93c0beea0c3
Jacob Perry <rakoryn@outlook.com>
2019-06-11 14:08:30 -0700

    Increased Whitty levels 1000%

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <div class="d-flex justify-content-center w-100 align-items-center my-4">
         <img
           src="@/assets/images/cartoon-jacob-04.jpg"
-          alt="Wizard Narwahl"
+          alt="Wizard Narwhal"
           class="img rounded-circle mt-3"
         />
       </div>
@@ -14,9 +14,11 @@
 
       <div class="about-text">
         <p>
-          You are probably looking at this site for one of a few reasons. Either you are trying to see if I am someone you think is worth hiring, or its my mom checking out my site to see how it looks. If it is my mom, Hi Mom, if not well I hope this site will help you figure out if I am someone you would want working for you, or at least maybe you will chuckle. Like when you watch those videos of a cat playing the piano, its just so adorable 😍. Wait, getting off topic... <em>Alright jacob we gotta look professional, pull it together!</em> Thank you for taking the time to check out my site. I appreciate the time you are here and please keep all hands, feet inside the vehicle at all times, oh and enjoy.
+          So you are probably looking at this site for one of two reasons. Either you are trying to see if I am someone you think is worth hiring, or... It's my mom checking out my site to see how it looks. If it's the latter, Hi Mom! But if not, well then I hope this site will help you figure out if I am someone you would want working for you! And at least maybe you will chuckle. Like when you watch those videos of a cat playing the piano... its just so adorable :heart_eyes:. Wait, getting off topic... <em>Alright Jacob, we have to look professional, pull it together!</em> In any case, thank you for taking the time to check out my site. Enjoy your stay here and please keep all hands and feet inside the vehicle at all times!
 
-          ...But really though, you should go check out those cat videos. 10/10, would watch again. You know what, here is a gif of it, just for you.
+          ...But really though, you should go check out those cat videos. 10/10, would watch again.
+
+          You know what, here is a gif of it, just for you.
         </p>
         <img
           src="https://media.giphy.com/media/10RhccNxPSaglW/giphy.gif"
